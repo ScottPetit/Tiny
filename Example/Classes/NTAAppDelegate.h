deb0e759090e0f174daba0910c30ccc8ec177d8d
Scott Petit <petit.scott@gmail.com>
2014-05-10 10:26:05 +0300

    Initial Commit - Project Init

---
+++ Example/Classes/NTAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NTAAppDelegate.h
+//  Tiny
+//
+//  Created by Scott Petit on 5/10/14.
+//  Copyright (c) 2014 Scott Petit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/Other Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tiny
+//
+//  Created by Scott Petit on 5/10/14.
+//  Copyright (c) 2014 Scott Petit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NTAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NTAAppDelegate class]));
+    }
+}
